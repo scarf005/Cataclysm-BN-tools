283ef3f2d0f3eabbb25fd194a24a9316b84b7ebc
scarf <greenscarf005@gmail.com>
2022-10-15 21:37:45 +0900

    refactor: slightly less cursed way to generate pydantic enums

--- schema/modinfo.py
+++ schema/modinfo.py
@@ -1,10 +1,9 @@
-from pprint import pprint
 import re
+from pprint import pprint
 from textwrap import dedent
-from typing import TYPE_CHECKING, Any, Literal, Type
+from typing import TYPE_CHECKING, Any, Literal, Type, Union
 
 from pydantic import BaseModel, Field, conlist, constr, create_model
-from pydantic.fields import FieldInfo
 
 if TYPE_CHECKING:
     ID = str
@@ -14,6 +13,7 @@ else:
     ID = constr(regex=r"^[a-zA-Z0-9_]+$")
     String = constr(min_length=1)
     List = conlist(min_items=1, item_type=String)
+
 
 def to_classname(text: str) -> str:
     return "".join(t.capitalize() for t in text.split("_"))
@@ -47,7 +47,9 @@ def field(*args: str, fold: bool = True, **kwargs: Any):
     )
 
 
-def enumModel(name: str, *args: str, fold: bool = True, **kwargs: Any):
+def enum_model(
+    name: str, *args: str, fold: bool = True, **kwargs: Any
+) -> Type[BaseModel]:
     """one args: description, two args: default, description"""
 
     return create_model(
@@ -57,52 +59,41 @@ def enumModel(name: str, *args: str, fold: bool = True, **kwargs: Any):
     )
 
 
-categories = {
-    "content": """A mod that adds a lot of stuff.
+def enums_from(data: dict[str, str]) -> Any:
+    categories_classes = (enum_model(k, v) for k, v in data.items())
+    return Union[*categories_classes]  # type: ignore
+
+
+Category = enums_from(
+    {
+        "content": """A mod that adds a lot of stuff.
                   Typically reserved for very large mods or
                   complete game overhauls
                   (eg: Core game files, Aftershock)""",
-    "items": """A mod that adds new items and recipes to the game
+        "items": """A mod that adds new items and recipes to the game
                 (eg: More survival tools)""",
-    "creatures": """A mod that adds new creatures or NPCs to the game
+        "creatures": """A mod that adds new creatures or NPCs to the game
                     (eg: Modular turrets)""",
-    "misc_additions": """Miscellaneous content additions to the game
+        "misc_additions": """Miscellaneous content additions to the game
                          (eg: Alternative map key, Crazy cataclysm)""",
-    "buildings": """New overmap locations and structures
+        "buildings": """New overmap locations and structures
                     (eg: Fuji's more buildings).
                     If you're blacklisting buildings from spawning,
                     this is also a usable category
                     (eg: No rail stations).""",
-    "vehicles": """New cars or vehicle parts
+        "vehicles": """New cars or vehicle parts
                    (eg: Tanks and  vehicles)""",
-    "rebalance": """A mod designed to rebalance the game in some way
+        "rebalance": """A mod designed to rebalance the game in some way
                     (eg: Safe autodocs).""",
-    "magical": """A mod that adds something magic-related to the game
+        "magical": """A mod that adds something magic-related to the game
                     (eg: Necromancy)""",
-    "item_exclude": """A mod that stops items from spawning in the world
+        "item_exclude": """A mod that stops items from spawning in the world
                     (eg: No survivor armor, No drugs)""",
-    "monster_exclude": """A mod that stops certain monster varieties from spawning in the world
+        "monster_exclude": """A mod that stops certain monster varieties from spawning in the world
                     (eg: No fungal monsters, No ants)""",
-    "graphical": """A mod that adjusts game graphics in some way
+        "graphical": """A mod that adjusts game graphics in some way
                     (eg: Graphical overmap)""",
-}
-for key, value in categories.items():
-    globals()[to_classname(key)] = enumModel(key, value)
-
-pprint(globals())
-
-Category = (
-    Content
-    | Items
-    | Creatures
-    | MiscAdditions
-    | Buildings
-    | Vehicles
-    | Rebalance
-    | Magical
-    | ItemExclude
-    | MonsterExclude
-    | Graphical
+    }
 )
 
 
@@ -111,13 +102,14 @@ class ModInfo(BaseModel):
         "MOD_INFO", "Identifier for ModInfo object.", alias="type", const=True
     )
     id_: ID = field(
-        """Mod's unique identifier, prefer to use only ASCII letters, numbers and underscore for clarity.""",
+        """Mod's unique identifier, prefer to use only ASCII letters,
+        numbers and underscore for clarity.""",
         alias="id",
     )
     name: String = field("Mod's display name, in `English`.")
     authors: List[String] = field("Original author(s) of the mod.")
     description: String = field("Mod's description, in `English`.")
-    category: Category = field(
+    category: Category = field(  # type: ignore
         """The `category` attribute denotes
         where the mod will appear in the mod selection menu.
         These are the available categories to choose from,
