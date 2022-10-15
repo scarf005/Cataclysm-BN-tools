9bdd6378f560170da50ac378fd2cfe8f4715cc1b
scarf <greenscarf005@gmail.com>
2022-10-16 00:33:47 +0900

    refactor: main file doesn't do anything (yet)

--- src/extension.ts
+++ src/extension.ts
@@ -8,24 +8,24 @@ export const activate = (context: vscode.ExtensionContext) => {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log(
-    'Congratulations, your extension "cataclysm-bright-nights-tools" is now active!'
+    'Congratulations, your extension "cataclysm-bn-tools" is now active!'
   )
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    'cataclysm-bright-nights-tools.helloWorld',
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage(
-        'Hello from Cataclysm: Bright Nights tools!'
-      )
-    }
-  )
+  // let disposable = vscode.commands.registerCommand(
+  //   'cataclysm-bn-tools.helloWorld',
+  //   () => {
+  //     // The code you place here will be executed every time your command is executed
+  //     // Display a message box to the user
+  //     vscode.window.showInformationMessage(
+  //       'Hello from Cataclysm: Bright Nights tools!'
+  //     )
+  //   }
+  // )
 
-  context.subscriptions.push(disposable)
+  // context.subscriptions.push(disposable)
 }
 
 // This method is called when your extension is deactivated
