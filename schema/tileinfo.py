dc8c56476bf90325dd97b423e6dc2b24727eb24c
scarf <greenscarf005@gmail.com>
2022-10-17 23:54:00 +0900

    feat: easier ID generation
    Now unique IDs with literal/prefix/suffixes can be easily converted into JSON Schema.

--- schema/tileinfo.py
+++ schema/tileinfo.py
@@ -1,73 +1,40 @@
-import re
-from typing import Type
+from pydantic import BaseModel
 
-from pydantic import BaseModel, ConstrainedStr, constr, create_model
+from schema.utils import ID, RegexType, field, id_from, reqfield
 
-from schema.utils import ID, enums_from, field, id_with, reqfield
+NPCS_DESC = """
+    {id} are used to identify
+    the sprites for the **player avatar** and **NPC**s."""
+NPCS = id_from(
+    "NPCS",
+    NPCS_DESC,
+    ("player_female", "player_male", "npc_female", "npc_male"),
+)
 
-NPC_ID_DESC = """ The special ids
-`"player_female"`, `"player_male"`, `"npc_female"`, `"npc_male"`
-are used to identify the sprites for the player avatar and NPCs."""
+SEASON_DESC = """
+    {id} can be applied to any entity id to create a **seasonal variant** for that entity that will be displayed in the appropriate season like this:
 
-SEASON_ID_DESC = """The special suffixes
-    `_season_spring`, `_season_summer`, `_season_autumn`, and `_season_winter`
-    can be applied to any entity id to create a seasonal variant for that entity
-    that will be displayed in the appropriate season like
-    this `"id": "mon_wolf_season_winter".`"""
+    "id": "mon_wolf_season_winter".
+    """
+SEASON = id_from(
+    "Season",
+    SEASON_DESC,
+    ("spring", "summer", "winter", "fall"),
+    RegexType.SUFFIX,
+    fold=False,
+)
 
-
-NPCS = {
-    k: NPC_ID_DESC
-    for k in ("player_female", "player_male", "npc_female", "npc_male")
-}
-
-UNKNOWN = {
-    "unknown": """The special id `"unknown"` provides a sprite
-                      that is displayed when an entity has no other sprite."""
-}
-
-
-# def id_with(suffix: str) -> Type[str]:
-#     return constr(regex=rf"^[a-zA-Z0-9_]+_{suffix}$")
-
-
-# SEASON = {
-#     id_with(k): SEASON_ID_DESC
-#     for k in (
-#         "season_spring",
-#         "season_summer",
-#         "season_autumn",
-#         "season_winter",
-#     )
-# }
-
-SpecialId = enums_from(NPCS | UNKNOWN)
-# SpecialIdSuffix = enums_from(SEASON)
-
-# SEASON_SPRING =
-# class IDWith1(ConstrainedStr):
-#     regex=re.compile( r"^[a-zA-Z0-9_]+_season_spring$")
-
-# class IDWith2(ConstrainedStr):
-#     regex=re.compile( r"^[a-zA-Z0-9_]+_season_winter$")
-
-IDWith1 = id_with("IDWith1", re.compile(r"^[a-zA-Z0-9_]+_season_spring$"))
-IDWith2 = id_with("IDWith2", re.compile(r"^[a-zA-Z0-9_]+_season_winter$"))
+UNKNOWN_DESC = """
+    {id} provides a sprite
+    that is displayed when an entity has no other sprite."""
+UNKNOWN = id_from("UNKNOWN", UNKNOWN_DESC, ("unknown",))
 
 
-class MySeasonSpring(BaseModel):
-    __root__: IDWith1 | IDWith2 = field(SEASON_ID_DESC)
-
-
-# SeasonSpring = create_model(
-#         "MyClassNameIsSeasonSpring",
-#         regex=re.compile(r"^[a-zA-Z0-9_]+_season_spring$"),
-#         __base__=ConstrainedStr,
-#     )
+SpecialID = NPCS | UNKNOWN | SEASON
 
 
 class TileEntry(BaseModel):
-    id_: ID | SpecialId | MySeasonSpring = reqfield(  # type: ignore
+    id_: ID | SpecialID = reqfield(  # type: ignore
         "the `game entity` represented by this sprite", alias="id"
     )
     fg: ID = reqfield("sprite name")
--- schema/utils.py
+++ schema/utils.py
@@ -1,6 +1,8 @@
 import re
+from enum import StrEnum
 from textwrap import dedent
 from typing import TYPE_CHECKING, Any, Literal, Type, Union
+from typing_extensions import LiteralString
 
 from pydantic import (
     BaseModel,
@@ -30,11 +32,11 @@ def fold_text(text: str) -> str:
     return re.sub(r"\s+", " ", text)  # type: ignore
 
 
-def get_desc(text: str, fold: bool = True):
-    return (fold_text if fold else dedent)(text)
+def get_desc(text: str, fold=True):
+    return (fold_text if fold else dedent)(text.lstrip())
 
 
-def field(*args: Any, fold: bool = True, **kwargs: Any):
+def field(*args: Any, fold=True, **kwargs: Any):
     """one args: description, two args: default, description"""
 
     desc = get_desc(args[-1], fold=fold)
@@ -57,12 +59,12 @@ def field(*args: Any, fold: bool = True, **kwargs: Any):
     return Field(**(args_dict | kwargs))
 
 
-def reqfield(desc: str, fold: bool = True, **kwargs: Any):
+def reqfield(desc: str, fold=True, **kwargs: Any):
     return field(..., desc, fold=fold, **kwargs, required=True)
 
 
 def enum_model(
-    name: str, *args: str, fold: bool = True, **kwargs: Any
+    name: str, *args: str, fold=True, **kwargs: Any
 ) -> Type[BaseModel]:
     """one args: description, two args: default, description"""
 
@@ -73,15 +75,61 @@ def enum_model(
     )
 
 
-def id_with(name: str, regex: re.Pattern[str]) -> Type[ConstrainedStr]:
+def enums_from(data: dict[str, str]) -> Any:
+    categories_classes = (enum_model(k, v) for k, v in data.items())
+    return Union[*categories_classes]  # type: ignore
+
+
+_ID = r"[a-zA-Z0-9_]+"
+
+
+class RegexType(StrEnum):
+    LITERAL = r"^{value}$"
+    PREFIX = rf"^{{value}}_{_ID}"
+    SUFFIX = rf"^{_ID}_{{value}}$"
+
+    @classmethod
+    def as_str(cls, regex: "RegexType", plural=False) -> str:
+        def pluralize(text: str, on_plural: str) -> str:
+            return text if not plural else f"{text}{on_plural}"
+
+        match regex:
+            case cls.LITERAL:
+                return pluralize("id", "s")
+            case cls.PREFIX:
+                return pluralize("prefix", "es")
+            case cls.SUFFIX:
+                return pluralize("suffix", "es")
+
+
+def one_of(texts: tuple[LiteralString, ...], regex: RegexType) -> str:
+    values = ", ".join(f"`{s}`" for s in texts)
+
+    return f"The special {regex.as_str(regex, len(texts) > 1)} {values}"
+
+
+def id_with(name: str, regex: re.Pattern[str]) -> Type["Model"]:  # type: ignore
     """creates a constrained regex string"""
-    return create_model( # type: ignore
+    return create_model(
         to_classname(name),
         regex=regex,
         __base__=ConstrainedStr,  # type: ignore
     )
 
 
-def enums_from(data: dict[str, str]) -> Any:
-    categories_classes = (enum_model(k, v) for k, v in data.items())
-    return Union[*categories_classes]  # type: ignore
+def id_from(
+    name: str,
+    template: str,
+    names: tuple[LiteralString, ...],
+    regex: RegexType = RegexType.LITERAL,
+    **kwargs: Any,
+):
+    desc = template.format(id=one_of(names, regex))
+    ids = [id_with(name, re.compile(regex.format(value=it))) for it in names]
+    IDsType = Union[*ids]  # type: ignore
+
+    return create_model(
+        to_classname(name),
+        __root__=(IDsType, field(desc, **kwargs)),
+        __base__=BaseModel,
+    )
